53dc43ca5482f0e94933fc29ff4f6211f7234f5f
William Paiva <williampaivabet@outlook.com>
2024-11-25 12:21:07 -0300

    3

--- 5_ACCOUNT_PROJECT/index.js
+++ 5_ACCOUNT_PROJECT/index.js
@@ -1,4 +1,38 @@
-const inquire = require('inquire')
-const fs = require('fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-console.log('Iniciando o projeto...')
+console.log('Iniciando o projeto...');
+
+operation();
+
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer',
+        choices: [
+            'Criar conta',
+            'Consultar saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    },])
+    .then((answer) => {
+        const action = answer['action'];
+        console.log(action);
+
+        if(action === "Criar Conta"){
+            createAccount()
+        }
+        
+    })
+    .catch((err) => {
+        console.error('Error:', err);
+    });
+}
+
+function createAccount(){
+    console.log('Defina as opções da sua conta a seguir...')
+}
+
